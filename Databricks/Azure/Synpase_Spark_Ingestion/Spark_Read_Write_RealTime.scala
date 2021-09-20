23583977d81da952f2f505eefb39eea0282018e1
Anjaiah Methuku <anjijava16@users.noreply.github.com>
2021-09-20 00:25:25 -0400

    Update Spark_Read_Write_RealTime.scala

--- Databricks/Azure/Synpase_Spark_Ingestion/Spark_Read_Write_RealTime.scala
+++ Databricks/Azure/Synpase_Spark_Ingestion/Spark_Read_Write_RealTime.scala
@@ -1,3 +1,14 @@
+// Defining the Service Principal credentials for the Azure storage account
+spark.conf.set("fs.azure.account.auth.type", "OAuth")
+spark.conf.set("fs.azure.account.oauth.provider.type",  "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+spark.conf.set("fs.azure.account.oauth2.client.id", "<application-id>")
+spark.conf.set("fs.azure.account.oauth2.client.secret", "<service-credential>")
+spark.conf.set("fs.azure.account.oauth2.client.endpoint", "https://login.microsoftonline.com/<directory-id>/oauth2/token")
+
+// Defining a separate set of service principal credentials for Azure Synapse Analytics (If not defined, the connector will use the Azure storage account credentials)
+spark.conf.set("spark.databricks.sqldw.jdbc.service.principal.client.id", "<application-id>")
+spark.conf.set("spark.databricks.sqldw.jdbc.service.principal.client.secret", "<service-credential>")
+
 # Set up the Blob storage account access key in the notebook session conf.
 spark.conf.set(
   "fs.azure.account.key.<your-storage-account-name>.blob.core.windows.net",
